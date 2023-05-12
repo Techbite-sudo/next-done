2400000c4b24e7e2670315f8f563129147ecdb5d
Techbite <bonfacemwema7@gmail.com>
2023-05-12 10:11:15 +0300

    APp layout

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,17 @@
+import Footer from "@/components/Footer"
+
+
 export default function About(){
     return(
         <div>About</div>
+    )
+}
+
+About.getLayout = function PageLayout(page){
+    return(
+        <>
+            {page}
+            <Footer/>
+        </>
     )
 }
